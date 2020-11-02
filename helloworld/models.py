16a418f3e8babed37518452c295d894ed0d68868
RookieCrazy <rookiecrazy@qq.com>
2020-11-02 23:52:55 +0800

    第二次测试

--- helloworld/models.py
+++ helloworld/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here 创建models.
 class People(models.Model):
     name = models.CharField(null=True, blank=True, max_length=200)
     job = models.CharField(null=True, blank=True, max_length=200)
