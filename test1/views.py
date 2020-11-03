e1e8bcdb983d4d272bfcaf9700bbae58bb94875c
RookieCrazy <rookiecrazy@qq.com>
2020-11-03 18:54:28 +0800

    {{}}模板变量

--- test1/views.py
+++ test1/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def test1(request):
-    return render(request, "test1/t1.html", {})
+    return render(request, "test1/t1.html", {
+        "title": "Test web",
+        "content": "ceshiceshiceshi718049519"
+    })
 
 
 def test2(request):
-    return render(request, "test1/t2.html", {})
+    return render(request, "test1/t1.html", {
+        "title": "test2",
+        "content": "jdoaphoieq938"
+    })
